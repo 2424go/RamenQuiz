b5d05b62a2987a50467742f34ac9e9d3312d26a6
kengo nishi <82163707+2424go@users.noreply.github.com>
2022-03-02 16:04:47 +0900

    Add files via upload

---
+++ RamenQuiz/QuizViewController.swift
@@ -0,0 +1,112 @@
+//
+//  QuizViewController.swift
+//  RamenQuiz
+//
+//  Created by Apple on 2022/02/26.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+    @IBOutlet var quizNumberLabel: UILabel!
+    @IBOutlet var quizTextView: UITextView!
+    @IBOutlet var answerButton1: UIButton!
+    @IBOutlet var answerButton2: UIButton!
+    @IBOutlet var answerButton3: UIButton!
+    @IBOutlet var answerButton4: UIButton!
+    @IBOutlet var judgeImageView: UIImageView!
+    
+    var csvArray: [String] = []
+    var quizArray: [String] = []
+    var quizCount = 0
+    var correctCount = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        csvArray = loadCSV(fileName: "quiz")
+        csvArray.shuffle()
+        print(csvArray)
+        
+        quizArray = csvArray[quizCount].components(separatedBy: ",")
+        quizNumberLabel.text = "第\(quizCount + 1)問"
+        quizTextView.text = quizArray[0]
+        answerButton1.setTitle(quizArray[2], for: .normal)
+        answerButton2.setTitle(quizArray[3], for: .normal)
+        answerButton3.setTitle(quizArray[4], for: .normal)
+        answerButton4.setTitle(quizArray[5], for: .normal)
+        
+        answerButton1.layer.borderWidth = 2
+        answerButton1.layer.borderColor = UIColor.black.cgColor
+        answerButton2.layer.borderWidth = 2
+        answerButton2.layer.borderColor = UIColor.black.cgColor
+        answerButton3.layer.borderWidth = 2
+        answerButton3.layer.borderColor = UIColor.black.cgColor
+        answerButton4.layer.borderWidth = 2
+        answerButton4.layer.borderColor = UIColor.black.cgColor
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue,sender: Any?) {
+        let scoreVC = segue.destination as! ScoreViewController
+        scoreVC.correct = correctCount
+    }
+    
+    //button action code
+    @IBAction func btnAction(sender: UIButton) {
+        if sender.tag == Int(quizArray[1]) {
+            correctCount += 1
+            print("正解")
+            judgeImageView.image = UIImage(named: "correct")
+        } else {
+            print("不正解")
+            judgeImageView.image = UIImage(named: "incorrect")
+        }
+        print("score:\(correctCount)")
+        judgeImageView.isHidden = false
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.judgeImageView.isHidden = true
+        }
+        nextQuiz()
+    }
+    
+    func nextQuiz() {
+        quizCount += 1
+        if quizCount < csvArray.count {
+            quizArray = csvArray[quizCount].components(separatedBy: ",")
+            quizNumberLabel.text = "第\(quizCount + 1)問"
+            quizTextView.text = quizArray[0]
+            answerButton1.setTitle(quizArray[2], for: .normal)
+            answerButton2.setTitle(quizArray[3], for: .normal)
+            answerButton3.setTitle(quizArray[4], for: .normal)
+            answerButton4.setTitle(quizArray[5], for: .normal)
+        } else {
+            performSegue(withIdentifier: "toScoreVC", sender: nil)
+        }
+    }
+    
+    func loadCSV(fileName: String) -> [String] {
+        let csvBundle = Bundle.main.path(forResource: fileName, ofType: "csv")!
+        do {
+            let csvData = try String(contentsOfFile: csvBundle,encoding: String.Encoding.utf8)
+            let lineChange = csvData.replacingOccurrences(of: "\r", with: "\n")
+            csvArray = lineChange.components(separatedBy: "\n")
+            csvArray.removeLast()
+        } catch {
+            print("error")
+        }
+        return csvArray
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
